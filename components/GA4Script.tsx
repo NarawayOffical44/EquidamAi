18e1d5d8867670dc9298c8717319fd1086aaf57d
aurjobsa <Aurjobsa@gmail.com>
2026-05-31 13:32:58 +0530

    fix: add direct analytics tags

--- components/GA4Script.tsx
+++ components/GA4Script.tsx
@@ -2,20 +2,16 @@
 
 import Script from "next/script";
 
-const DEFAULT_GA4_MEASUREMENT_ID = "G-TPJBBP9TKQ";
-
 export function GA4Script() {
-  const measurementId = process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID || DEFAULT_GA4_MEASUREMENT_ID;
-
   return (
     <>
       <Script
-        id="ga4-script"
+        id="google-tag-script"
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-TPJBBP9TKQ"
       />
       <Script
-        id="ga4-init"
+        id="google-tag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -23,7 +19,20 @@ export function GA4Script() {
             function gtag(){dataLayer.push(arguments);}
             window.gtag = gtag;
             gtag('js', new Date());
-            gtag('config', '${measurementId}');
+            gtag('config', 'G-TPJBBP9TKQ');
+          `,
+        }}
+      />
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wyaovo5ibf");
           `,
         }}
       />
